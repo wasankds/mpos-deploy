bcce58b3fc227358e60e6866755e37616e32cf35
wasankds <wasankds@gmail.com>
2025-09-28 06:40:05 +0700

    Sun 09/28/2025  6:40:05.54

--- mpos.js
+++ mpos.js
@@ -1,38 +1,29 @@
 // await import(`./${mymoduleFolder}/myDbStart.js`)  // แยกไปทำตอนเปิดระบบครั้งแรก
+// import { MongoClient } from 'mongodb' ; 
 import 'dotenv/config' ; 
 import express from 'express' ; 
 import session from 'express-session' ; 
 import cookieParser from 'cookie-parser' ; 
 import flash from 'connect-flash' ; 
-import { MongoClient } from 'mongodb' ; 
 import MongoDBSession from 'connect-mongodb-session'  ; 
 global.IS_PRODUCTION = process.env.IS_PRODUCTION == 1 ? true : false ;
 global.PROJECT_DIR = process.cwd()
+const PORT = process.env.PORT_DEPLOY == 0 ? process.env.PORT_DEV : process.env.PORT_SERVER
+global.DOMAIN_ALLOW = process.env.PORT_DEPLOY == 0 ? `${process.env.LOCALHOST_ALLOW}:${PORT}` : `${process.env.DOMAIN_ALLOW}`
 global.dbName = process.env.DB_NAME
 global.dbUrl = process.env.DB_URL
 global.mymoduleFolder = global.IS_PRODUCTION ? 'mymodule-min' : 'mymodule'
 const routesFolder = global.IS_PRODUCTION ? 'routes-min' : 'routes'
-await import(`./${mymoduleFolder}/myGlobal.js`)   // กำหนด global ตัวแปรต่างๆ
+await import(`./${mymoduleFolder}/myGlobal.js`)
 await import(`./${mymoduleFolder}/mySchedule.js`) // รันปิดเอกสารตอนตี2ทุกวัน 
 const app = express()
-async function loadSettingSystemStart() {
-  const client = new MongoClient(global.dbUrl)
-  await client.connect()
-  const db = client.db(global.dbName)
-  const findSettingSystem = await db.collection(global.dbColl_settingsSystem).findOne({})
-  await client.close()
-  return findSettingSystem ? findSettingSystem  : global.SYSTEM_START
-}
-global.SETTINGS_SYSTEM = await loadSettingSystemStart()
-const PORT = SETTINGS_SYSTEM.DEPLOY == 0 ? SETTINGS_SYSTEM.PORT_DEV : SETTINGS_SYSTEM.PORT_SERVER
-global.DOMAIN_ALLOW = SETTINGS_SYSTEM.DEPLOY == 0 ? `${SETTINGS_SYSTEM.LOCALHOST_ALLOW}:${PORT}` : `${SETTINGS_SYSTEM.DOMAIN_ALLOW}`
 const MongoStore = MongoDBSession(session)
 app.use(session({
   secret: 'mpos.node.apps.key.sign.cookie',
   cookie: {
-    maxAge: 1000*60*60*24*30, // 30 days
+    maxAge: 1000*60*60*24*7, // 7 days
     // Prevent client-side JavaScript from accessing the cookie
-    // secure: process.env.DEPLOY == 'dev' ? false : true, // ตัวนี้ทำให้มีปัญหาให้ปิดไป
+    // secure: process.env.PORT_DEPLOY == 'dev' ? false : true, // ตัวนี้ทำให้มีปัญหาให้ปิดไป
     httpOnly: IS_PRODUCTION ? true : false ,
   },
   resave: false ,
@@ -46,13 +37,13 @@ app.use(session({
 app.set('view engine', 'ejs')
 app.use(flash())
 app.use(cookieParser())
-app.use(express.json({limit:'50mb'})) // 50mb กรณีอัปโหลดไฟล์จาก fetch ต้องอนุญาตไฟล์ที่ใหญ่ขึ้น
+app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({extended:true,limit:'50mb'}))
-app.use(express.static(global.folderPublic)) // ถ้าไม่มีจะหา public ไม่เจอ
+app.use(express.static(global.folderPublic))
 app.use((req, res, next) => {
   const allowedOrigins = [ 
-    SETTINGS_SYSTEM.DOMAIN_ALLOW, // สำหรับใช้งานจริง
-    `${SETTINGS_SYSTEM.LOCALHOST_ALLOW}:${PORT}` , // สำหรับทดสอบยิง fetch บนเครื่องตัวเอง 
+    global.DOMAIN_ALLOW, // สำหรับใช้งานจริง
+    `${process.env.LOCALHOST_ALLOW}:${PORT}` , // สำหรับทดสอบยิง fetch บนเครื่องตัวเอง 
     // 'http://127.0.0.1:5500' // สำหรับทดสอบยิง fetch จากไฟล์ html ที่ใช้ live server
   ]
   const origin = req.headers.origin
@@ -64,7 +55,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-if( process.env.USE_STARTAPP_ROUTER == 1 ) { // เปิดใช้ตอนเซ็ตระบบเท่านั้น เปิด/เปิดใน .env
+if( process.env.USE_STARTAPP_ROUTER == 1 ) {
   app.use((await import(`./${routesFolder}/startAppRouter.js`)).default) 
 }
 app.use((await import(`./${routesFolder}/homeRouter.js`)).default) 
@@ -82,7 +73,7 @@ app.use((await import(`./${routesFolder}/suppliersRouter.js`)).default);
 app.use((await import(`./${routesFolder}/docGeneralRouter.js`)).default)
 app.use((await import(`./${routesFolder}/docMainRouter.js`)).default);
 app.use((await import(`./${routesFolder}/reportDocsRouter.js`)).default);
-app.use((await import(`./${routesFolder}/reportItemsRouter.js`)).default); // ยังไม้่ได้ทำ - สำหรับเวอร์ชั่นจ่ายเงิน
+app.use((await import(`./${routesFolder}/reportItemsRouter.js`)).default);
 app.use( (err, req, res, next) => {
   res.status(err.status || 500);
   const errHtml = `<h1 style="color:blue">กำลังอัปเดทข้อมูล</h1>
@@ -94,15 +85,8 @@ app.use( (err, req, res, next) => {
 app.get('*', (req,res) => {
   res.status(404).sendFile(file404)
 })
-//=== Start the server
 app.listen(PORT, () => {
   console.log(`========== Server@${DOMAIN_ALLOW} ===========`)
   console.log("- IS_PRODUCTION ", global.IS_PRODUCTION)
-  console.log("- global.DOMAIN_ALLOW ", global.DOMAIN_ALLOW)
+  console.log("- DOMAIN_ALLOW ", global.DOMAIN_ALLOW)
 })
-
-
-
-
-
-
